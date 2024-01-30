b5b72f52ad84aa3a1597342508e08b02c40c9c64
NoorA34 <nali@bayviewglen.ca>
2024-01-30 15:52:56 -0500

    o

---
+++ Horse Racing Assignment/src/horseracing/Betting.java
@@ -0,0 +1,19 @@
+package horseracing;
+
+public class Betting {
+ /*public int odds(Horse horse) {
+
+        int c = Race.class(int)(7 - Math.abs(Race.horse.getPreferredLength() - Race.raceLength));
+
+        if (Race.class.raceSurface.equalsIgnoreCase("grass"))
+            c += horse.getGrassRating() / 2;
+        else if (raceSurface.equalsIgnoreCase("dirt"))
+            c += horse.getDirtRating() / 2;
+        else if (raceSurface.equalsIgnoreCase("mud"))
+            c += horse.getMudRating() / 2;
+       
+    };
+*/
+
+}
+//Math.random()*(7-preferred-length)
--- Horse Racing Assignment/src/horseracing/Race.java
+++ Horse Racing Assignment/src/horseracing/Race.java
@@ -49,17 +49,24 @@ public class Race {
     }
 
     public void displayHorseTable(){
+        System.out.printf("|%-15s|%-20s|%-15s|%-15s|%-15s|%-15s|%-15s|%-15s|%-15s|\n", "#","Horse Name", "Dirt Rating (%)", "Grass Rating (%)", "Mud Rating (%)", "Preferred Length", "Odds for Win", "Odds for Place", "Odds for Show");
         for (int i = 0; i < horses.size(); i++) {   // iterates through the horses list
             Horse horse = horses.get(i);
+            String s0 = "" + (i + 1);
             String s1 = "" + horse.getName();
             String s2 = "" + horse.getDirtRating();
             String s3 = "" + horse.getGrassRating();
             String s4 = "" + horse.getMudRating();
+            String s5 = "" + horse.getPreferredLength();
+            String s6 = "" + fraction(odds(horse));
+            String s7 = "" ;
+            String s8 = "";
 
-            System.out.println("+--------------------+-----+-----+-----+");
-            System.out.printf("|%-20s|%5s|%5s|%5s|\n", s1, s2, s3, s4);
+            
+            System.out.println("+---------------+--------------------+---------------+----------------+---------------+---------------+--------------+---------------+---------------+");
+            System.out.printf("|%-15s|%-20s|%-15s|%-15s|%-15s|%-15s|\n", s0, s1, s2, s3, s4, s5, s6, s7, s8);
         }
-        System.out.println("+--------------------+-----+-----+-----+");
+        System.out.println("+---------------+--------------------+---------------+----------------+---------------+---------------+--------------+---------------+---------------+");
     }
 
     public void displayRaceInfo() {
@@ -115,7 +122,7 @@ public class Race {
     }
     // Other methods for simulating the race, calculating winners, etc., can be added as needed
 
-    private int getIncrementForHorse(Horse horse) {
+    public int getIncrementForHorse(Horse horse) {
 
         int d = (int)(7 - Math.abs(horse.getPreferredLength() - this.raceLength));
 
@@ -128,8 +135,27 @@ public class Race {
         // this.raceSurface
        return (int)(Math.random() * d);
     }
+   public int odds(Horse horse) {
+        
+        int c = (int)(7 - Math.abs(horse.getPreferredLength() - this.raceLength));
 
+        if (raceSurface.equalsIgnoreCase("grass"))
+            c += horse.getGrassRating() * 10 / 2;
+        else if (raceSurface.equalsIgnoreCase("dirt"))
+            c += horse.getDirtRating() * 10 / 2;
+        else if (raceSurface.equalsIgnoreCase("mud"))
+            c += horse.getMudRating() * 10 / 2;
+        float total =  (c + (horse.getGrassRating() * 10 + horse.getDirtRating() * 10 + horse.getMudRating() * 10) / 12 );
+        float oddsValue =  Math.round((2 * Math.pow(100 / total, 2)) - 1);
+    return (int) oddsValue;
+       
+    }
+    public String fraction ( int oddsValue){
 
+    return oddsValue + "-1";
+    }
+
+   
     private void resetHorses() {
         for (Horse horse : horses) {
             horse.resetCurrenPosition();
